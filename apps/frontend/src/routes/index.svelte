6b9f47a50dc16644046b74c234d071464099bf33
jacksloan <jbsloan10@gmail.com>
2021-11-27 14:28:55 -0600

    npm run build on api-client package

--- apps/frontend/src/routes/index.svelte
+++ apps/frontend/src/routes/index.svelte
@@ -1,2 +1,18 @@
+<script context="module" lang="ts">
+  import * as api from 'asdf';
+  export async function load({ fetch }) {
+    const response = await fetch('index.json');
+    const posts = await response.json();
+    return { posts };
+  }
+</script>
+
+<script>
+  export let posts;
+  $: console.log(posts);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
