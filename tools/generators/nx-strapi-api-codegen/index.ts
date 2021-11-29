010f46a7347a873bf525c43bca373835658ca9d6
Jack Sloan <jbsloan10@gmail.com>
2021-11-28 18:47:14 -0600

    testing run commands

--- tools/generators/nx-strapi-api-codegen/index.ts
+++ tools/generators/nx-strapi-api-codegen/index.ts
@@ -1,30 +1,27 @@
 import {
   formatFiles,
   generateFiles,
-  getWorkspacePath,
   installPackagesTask,
   joinPathFragments,
   readProjectConfiguration,
   Tree,
 } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
-import { execSync } from 'child_process';
+import runCommands from '@nrwl/workspace/src/generators/run-commands/run-commands';
 
 export default async function (tree: Tree, schema: any) {
   console.log('Creating strapi api library');
-  const root = getWorkspacePath(tree);
+  await libraryGenerator(tree, { name: schema.name });
   const libraryRoot = readProjectConfiguration(tree, schema.name).root;
-  const inputFile = joinPathFragments(root, schema.openapi);
-  const output = joinPathFragments(root, libraryRoot, 'src', 'lib', 'codegen');
+  const inputFile = joinPathFragments(schema.openapi);
+  const output = joinPathFragments(libraryRoot, 'src', 'lib', 'codegen');
 
   console.log({
-    root,
     libraryRoot,
     inputFile,
     output,
   });
 
-  await libraryGenerator(tree, { name: schema.name });
   generateFiles(
     tree, // the virtual file system
     joinPathFragments(__dirname, './files'), // path to the file templates
@@ -32,7 +29,11 @@ export default async function (tree: Tree, schema: any) {
     schema // config object to replace variable in file templates
   );
 
-  execSync(`npx openapi-typescript-codegen -i ${inputFile} -o ${output}`);
+  runCommands(tree, {
+    command: `npx openapi-typescript-codegen -i ${inputFile} -o ${output}`,
+    name: 'x',
+    project: schema.name,
+  });
 
   await formatFiles(tree);
 
